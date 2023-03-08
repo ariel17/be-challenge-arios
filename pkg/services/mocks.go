3885af46d8dff11e1bb3c565d55d67c64cf9c9c0
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-03-08 02:34:44 -0300

    Adds missing mocks.

--- pkg/services/mocks.go
+++ pkg/services/mocks.go
@@ -2,6 +2,8 @@ package services
 
 import (
 	"github.com/stretchr/testify/mock"
+
+	"github.com/ariel17/be-challenge-arios/pkg/models"
 )
 
 type MockImporterService struct {
@@ -17,9 +19,26 @@ type MockStatusService struct {
 	mock.Mock
 }
 
-// GetStatus checks the application's health and returns and object describing
-// it.
 func (m *MockStatusService) GetStatus() Status {
 	args := m.Called()
 	return args.Get(0).(Status)
+}
+
+type MockFootballService struct {
+	mock.Mock
+}
+
+func (m *MockStatusService) GetPlayersByCompetitionCode(code, teamNameToFilter string) ([]models.Person, bool, error) {
+	args := m.Called(code, teamNameToFilter)
+	return args.Get(0).([]models.Person), args.Bool(1), args.Error(2)
+}
+
+func (m *MockStatusService) GetTeamByTLA(tla string, withPlayers bool) (*models.Team, error) {
+	args := m.Called(tla, withPlayers)
+	return args.Get(0).(*models.Team), args.Error(1)
+}
+
+func (m *MockStatusService) GetPersonsByTeamTLA(tla string) ([]models.Person, error) {
+	args := m.Called(tla)
+	return args.Get(0).([]models.Person), args.Error(1)
 }
